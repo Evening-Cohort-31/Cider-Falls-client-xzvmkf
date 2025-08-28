b6ad39c071555cb44ad1dc2c4e7753165552bdf8
Nick Segui <nicksegui5@gmail.com>
2025-08-28 18:37:31 -0500

    created function to generate list of guests

--- scripts/guests.js
+++ scripts/guests.js
@@ -1,1 +1,20 @@
 // Define function to create HTMl for list of guests
+import { getGuests } from "./databaser.js";
+
+export const guestList = () => {
+  const guests = getGuests();
+
+  let guestHTML = "";
+  guestHTML += "<ul class=`guest-list`>";
+
+  for (const guest of guests) {
+    guestHTML += (
+      <li class="guest-item">
+        ${guest.firstName} ${guest.lastName}
+      </li>
+    );
+  }
+
+  guestHTML += "</ul>";
+  return guestHTML;
+};
