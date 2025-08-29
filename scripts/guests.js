b1eabcf5f30a3c7d92f73c6c7f8e702e254e76d8
Nick Segui <nicksegui5@gmail.com>
2025-08-28 19:59:34 -0500

    syntax errors fixed

--- scripts/guests.js
+++ scripts/guests.js
@@ -5,16 +5,14 @@ export const guestList = () => {
   const guests = getGuests();
 
   let guestHTML = "";
-  guestHTML += "<ul class=`guest-list`>";
+  guestHTML += `<ul class="guest-list">`;
 
   for (const guest of guests) {
-    guestHTML += (
-      <li class="guest-item">
+    guestHTML += `<li class="guest-item">
         ${guest.firstName} ${guest.lastName}
-      </li>
-    );
+      </li>`;
   }
 
-  guestHTML += "</ul>";
+  guestHTML += `</ul>`;
   return guestHTML;
 };
