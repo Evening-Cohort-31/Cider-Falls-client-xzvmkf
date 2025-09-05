730e85387fe0bea2662bb480beb01e4a2845d23a
Erin Telfer <eringtelfer@gmail.com>
2025-09-05 18:44:26 -0500

    commented out what we are going to do in the new module I made

---
+++ scripts/services.js
@@ -0,0 +1,3 @@
+// import the structured clone of the array of services from the database module.
+// define a function that displays a list of all of the services offered throughout the different areas of the park.
+// create an event listener that when a service in that list is clicked on, displays which areas of the park that service is offered in. 
